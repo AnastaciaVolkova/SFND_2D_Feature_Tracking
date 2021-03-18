9e7c343eeb78950c69e8067c9e6d37843805c8b5
AnastaciaVolkova <polarbolide@gmail.com>
2021-03-18 17:50:39 +0100

    chore: Add packing shell

---
+++ pack_me.sh
@@ -0,0 +1,3 @@
+zip -r SFND_2D_Feature_Tracking.zip . -x '.git/*' '.vscode/*' 'utils/*' 'build/*' 'pack_me.sh'
+zip -urj SFND_2D_Feature_Tracking.zip ./utils/detectors.xlsx ./utils/matchers.xlsx ./utils/timing.xlsx
+
